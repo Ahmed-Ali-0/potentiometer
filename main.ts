13d769b3c5d5ca845d98e71746790bc668f54522
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-10-07 12:45:01 -0400

    Add Potentiometer

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+// Created by: Ahmed
+// Created date :Oct 7, 2020.
+// 
+// This program use Potentiometer.
 basic.forever(function () {
-	
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    basic.pause(1000)
 })
